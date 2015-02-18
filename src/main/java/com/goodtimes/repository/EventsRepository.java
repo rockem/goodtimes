8ccd91944730f2df9855cfabd0267ef7404ba564
Eli Segal <eli.segal@gmail.com>
2015-02-18 21:59:56 +0200

    Added MongoDB support for spring

--- src/main/java/com/goodtimes/repository/EventsRepository.java
+++ src/main/java/com/goodtimes/repository/EventsRepository.java
@@ -1,12 +1,12 @@
 package com.goodtimes.repository;
 
 import com.goodtimes.entity.GoodtimeEvent;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by rockem on 16/2/15.
  */
-public interface EventsRepository extends CrudRepository<GoodtimeEvent, Long> {
+public interface EventsRepository extends MongoRepository<GoodtimeEvent, Long> {
 
-    //GoodtimeEvent save(GoodtimeEvent event);
 }
