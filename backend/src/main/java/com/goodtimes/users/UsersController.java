7c5cd3fa362e11aa84589f9be80223c7bbd9dc7e
Eli Segal <eli.segal@gmail.com>
2015-04-25 22:54:25 +0300

    Basic security in progress

--- backend/src/main/java/com/goodtimes/users/UsersController.java
+++ backend/src/main/java/com/goodtimes/users/UsersController.java
@@ -1,5 +1,7 @@
 package com.goodtimes.users;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UsersController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public void signup(@RequestBody String body) {
 
+    @RequestMapping(method = RequestMethod.GET)
+    public String findAll() {
+        return "Eli";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> signup(@RequestBody String body) {
+        return new ResponseEntity<Object>(HttpStatus.CREATED);
     }
 }
